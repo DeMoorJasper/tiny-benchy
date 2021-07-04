2b3b57aa238812adc5604b72c6802028fadea144
Jasper De Moor <jasperdemoor@gmail.com>
2021-07-04 21:15:46 +0200

    a lot more stats

--- src/stats.ts
+++ src/stats.ts
@@ -1,3 +1,43 @@
+// Based on https://github.com/bestiejs/benchmark.js
+
+/**
+ * T-Distribution two-tailed critical values for 95% confidence.
+ * For more info see http://www.itl.nist.gov/div898/handbook/eda/section3/eda3672.htm.
+ */
+const tTable = {
+  "1": 12.706,
+  "2": 4.303,
+  "3": 3.182,
+  "4": 2.776,
+  "5": 2.571,
+  "6": 2.447,
+  "7": 2.365,
+  "8": 2.306,
+  "9": 2.262,
+  "10": 2.228,
+  "11": 2.201,
+  "12": 2.179,
+  "13": 2.16,
+  "14": 2.145,
+  "15": 2.131,
+  "16": 2.12,
+  "17": 2.11,
+  "18": 2.101,
+  "19": 2.093,
+  "20": 2.086,
+  "21": 2.08,
+  "22": 2.074,
+  "23": 2.069,
+  "24": 2.064,
+  "25": 2.06,
+  "26": 2.056,
+  "27": 2.052,
+  "28": 2.048,
+  "29": 2.045,
+  "30": 2.042,
+  infinity: 1.96,
+};
+
 export const round = (val, points = 3) => {
   return Math.round(val * 10 ** points) / 10 ** points;
 };
@@ -19,39 +59,78 @@ export const formatTime = (val) => {
 };
 
 export class Stats {
-  xs: Array<number>;
-  x0: number;
-  x1: number;
+  timings: Array<number>;
+  sampleCount: number;
+  totalDuration: number;
   x2: number;
 
   constructor() {
-    this.xs = [];
-    this.x0 = this.x1 = this.x2 = 0;
+    this.timings = [];
+    this.sampleCount = 0;
+    this.totalDuration = 0;
+    this.x2 = 0;
   }
 
-  add(x) {
-    this.xs.push(x);
-    this.x0 += 1;
-    this.x1 += x;
-    this.x2 += x * x;
+  add(timing: number) {
+    this.timings.push(timing);
+    this.sampleCount += 1;
+    this.totalDuration += timing;
+    this.x2 += timing * timing;
   }
 
   samples() {
-    return this.x0;
+    return this.sampleCount;
   }
 
   total() {
-    return this.x1;
+    return this.totalDuration;
   }
 
+  // Compute the sample mean (estimate of the population mean).
   mean() {
-    return this.x1 / this.x0;
+    return this.totalDuration / this.sampleCount;
   }
 
+  // Compute the sample variance (estimate of the population variance).
+  variance() {
+    const mean = this.mean();
+    const sqDiff = this.timings.reduce((acc, timing) => {
+      return acc + (timing - mean) * (timing - mean);
+    }, 0);
+    return sqDiff / this.sampleCount;
+  }
+
+  // Compute the sample standard deviation (estimate of the population standard deviation).
   stddev() {
-    return Math.sqrt(this.x0 * this.x2 - this.x1 * this.x1) / (this.x0 - 1);
+    return Math.sqrt(this.variance());
   }
 
+  // Compute the standard error of the mean (a.k.a. the standard deviation of the sampling distribution of the sample mean).
+  stdErrorMean() {
+    return this.stddev() / Math.sqrt(this.sampleCount);
+  }
+
+  // Compute the degrees of freedom.
+  degreeFreedom() {
+    return this.sampleCount - 1;
+  }
+
+  // Compute the critical value.
+  critical() {
+    return tTable[Math.round(this.degreeFreedom()) || 1] || tTable.infinity;
+  }
+
+  // Compute the margin of error.
+  marginOfError() {
+    return this.stdErrorMean() * this.critical();
+  }
+
+  // Compute the relative margin of error.
+  relativeMarginOfError() {
+    return (this.marginOfError() / this.mean()) * 100 || 0;
+  }
+
+  // Compute operations per second based on mean
   opsPerSec() {
     return 1e9 / this.mean();
   }
@@ -59,9 +138,9 @@ export class Stats {
   toString() {
     return `${Math.round(
       this.opsPerSec()
-    ).toLocaleString()} opts/sec (mean: ${formatTime(
-      this.mean()
-    )}, stddev: ${formatTime(
+    ).toLocaleString()} opts/sec, ±${this.relativeMarginOfError().toPrecision(
+      2
+    )}% (mean: ${formatTime(this.mean())}, stddev: ${formatTime(
       this.stddev()
     )}, ${this.samples().toLocaleString()} samples)`;
   }
